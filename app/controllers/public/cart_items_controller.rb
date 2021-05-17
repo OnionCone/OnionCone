037590d97614202ae3aeb6c7aeeeb33040d8561e
KURUMI-HARAPECO <kurumilk509@gmail.com>
2021-05-17 05:32:54 +0000

    LINKだけ

---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,33 @@
+class Public::CartItemsController < ApplicationController
+  def index
+    # @cart_item = Cart_items.find(params[:id])
+    # @cart_item = Cart_items.all
+  end
+
+#   def destroy
+#     @cart_item = Cart_items.find(params[:id])  # データ（レコード）を1件取得
+#     @cart_item.destroy  # データ（レコード）を削除
+#     redirect_to cart_item_path(@cart_item.id)  # 投稿一覧画面へリダイレクト
+#   end
+
+#   def destroy_all
+#     @cart_item = Cart_items.all
+#     @cart_item.destroy
+#     redirect_to cart_item_path(@cart_item.id)
+#   end
+
+#   def create
+#   end
+
+#   def update
+#     @cart_item = Cart_items.find(params[:id])
+#     @cart_item.update(cart_item_params)
+#     redirect_to cart_item_path(@cart_item.id)
+#   end
+
+
+#   private
+#   def cart_item_params
+#     params.require(:cart_items).permit(:amount)
+#   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
       passwords: 'public/customers/passwords'
     }
     resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items' => 'cart_items#destroy_all', as: 'cart_item_all'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
