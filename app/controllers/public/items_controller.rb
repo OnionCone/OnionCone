4a1ee28902f889e5bbe3ae91724495abbc162c6d
KURUMI-HARAPECO <kurumilk509@gmail.com>
2021-05-21 00:45:26 +0000

    ordersとcart_itemsがまだ

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -4,9 +4,9 @@ class Public::ItemsController < ApplicationController
     @items = Item.page(params[:page]).per(8)
   end
   def show
-  @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   # @current_item_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+  # @categories = OrderDetail.all
   end
-
 end
