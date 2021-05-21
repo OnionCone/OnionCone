f502bc762987280558693a3e7849d90cfcf429a9
KURUMI-HARAPECO <kurumilk509@gmail.com>
2021-05-21 00:58:13 +0000

    ordersとcart_itemsがまだ

--- config/routes.rb
+++ config/routes.rb
@@ -17,14 +17,14 @@ Rails.application.routes.draw do
     resources :orders, only: [:index, :show]
   end
   devise_for :customers
-  # scope module: 'public' do
-  #   root to: 'homes#top'
-  #   get '/about', to: 'homes#about'
-  #   get 'customers/my_page', to: 'customers#show'
-  #   get 'customers/unsubscribe', to: 'customers#unsubscribe'
-  #   patch 'customers/withdrawal', to: 'customers#withdrawal', as: 'withdrawal'
-  #   resources :customers, only: [:edit, :update]
-  # end
+  scope module: 'public' do
+    root to: 'homes#top'
+    get '/about', to: 'homes#about'
+    get 'customers/my_page', to: 'customers#show'
+    get 'customers/unsubscribe', to: 'customers#unsubscribe'
+    patch 'customers/withdrawal', to: 'customers#withdrawal', as: 'withdrawal'
+    resources :customers, only: [:edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # namespace :admin do
   #   resources :items, only: [:new, :create, :edit, :update]
