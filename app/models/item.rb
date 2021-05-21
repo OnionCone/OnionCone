27d3823fdb0d0934f359cf564b524448630ff12c
EC2 Default User <ec2-user@ip-172-31-0-96.ec2.internal>
2021-05-21 07:38:22 +0000

    item修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   validates :is_deleted, inclusion: { in: [true, false]}
 
-  belongs_to :genre, optional: true
+  belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   attachment :image
