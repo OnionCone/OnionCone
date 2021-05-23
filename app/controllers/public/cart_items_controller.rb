e0ac096400072c3b044037c56786598f0941e35b
KURUMI-HARAPECO <kurumilk509@gmail.com>
2021-05-23 02:05:02 +0000

    routes修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,25 +7,25 @@ class Public::CartItemsController < ApplicationController
   def destroy
     @cart_item = CartItem.find(params[:id])  # データ（レコード）を1件取得
     @cart_item.destroy  # データ（レコード）を削除
-    redirect_to public_cart_items_path  # 投稿一覧画面へリダイレクト
+    redirect_to cart_items_path  # 投稿一覧画面へリダイレクト
   end
 
   def destroy_all
     CartItem.destroy_all
-    redirect_to public_cart_items_path
+    redirect_to cart_items_path
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.save
-    redirect_to public_cart_items_path
+    redirect_to cart_items_path
   end
 
   def update
     cart_item = CartItem.find(params[:id])
     cart_item.update(cart_item_params)
-    redirect_to public_cart_items_path
+    redirect_to cart_items_path
   end
 
   private
