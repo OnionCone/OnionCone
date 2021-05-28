38eb9d2ef06ad531226a353208dda88e451dd006
EC2 Default User <ec2-user@ip-172-31-77-240.ec2.internal>
2021-05-28 06:05:37 +0000

    動画撮影用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,16 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
       when Customer
-        root_path
+        customers_my_page_path
       when Admin
         admin_root_path
     end
   end
-  
+
   def after_sign_out_path_for(resource)
     if resource == :customer
-        new_customer_session_path
+        new_admin_session_path
+        #new_customer_session_path
     elsif resource == :admin
         new_admin_session_path
     else
--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,8 +2,7 @@ class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
   def index
     @address = Address.new
-    @addresses = Address.all
-    @customer = current_customer
+    @addresses = current_customer.addresses
   end
 
   def create
